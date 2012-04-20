40c9e5a6211bf5a618b26ec91375cec96fa05e74
Bas van Manen <basje1@gmail.com>
2012-04-20 18:54:20 +0200

    added php doc and typehint array at checkRoute

--- libraries/bwork/router/handler/interface.php
+++ libraries/bwork/router/handler/interface.php
@@ -1,16 +1,43 @@
 <?php
+/**
+ * Bwork Framework
+ *
+ * @package Bwork
+ * @subpackage Bwork_Router
+ * @author Bas van Manen <basje1[at]gmail.com>
+ * @version $id: Bwork Framework v 0.1
+ * @license http://creativecommons.org/licenses/by-nc-sa/3.0/
+ */
 
+/**
+ * Interface
+ *
+ * The interface for Route handlers
+ *
+ * @package Bwork
+ * @subpackage Bwork_Router
+ * @version v 0.1
+ */
 interface Bwork_Router_Handler_Interface {
     
     /**
      * Will attempt to resolve if there is a specified route set for parameter $url
-     * @param $url string
+     * @param Array $url
      * @return boolean
      */
-    public function checkRoute($url);
+    public function checkRoute(array $url);
     
     /**
      * Will return the controller/action/mockparams parameters in array format
+     * 
+     * Example:
+     * <code>
+     * return array(
+     *  'controller'    => 'Controllername',
+     *  'action'        => 'ActionName',
+     *  'mockParams'    => array('1', '2')
+     * );
+     * </code>
      * @return array
      */
     public function getParams();
