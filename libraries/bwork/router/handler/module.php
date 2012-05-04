7bf797ff4100ec9e87daae2b5eec10058fd28bb2
Bas van Manen <basje1@gmail.com>
2012-05-04 17:31:49 +0200

    Edit on style

--- libraries/bwork/router/handler/module.php
+++ libraries/bwork/router/handler/module.php
@@ -18,7 +18,8 @@
  * @subpackage Bwork_Router_Handler
  * @version v 0.1
  */
-final class Bwork_Router_Handler_Module implements Bwork_Router_Handler_Interface {
+final class Bwork_Router_Handler_Module implements Bwork_Router_Handler_Interface
+{
     
     /**
      * Will store the parameter gained when resolving a route
@@ -29,28 +30,30 @@ final class Bwork_Router_Handler_Module implements Bwork_Router_Handler_Interfac
     /** 
      * @see Bwork_Router_Handler_Interface::checkRoute()
      */
-    public function checkRoute(array $url) {
-    	$config = Bwork_Core_Registry::getInstance()->getResource('Bwork_Config_Confighandler');
-		$moduleManager = Bwork_Core_Registry::getInstance()->getResource('Bwork_Module_Manager');
+    public function checkRoute(array $url)
+    {
+        $config        = Bwork_Core_Registry::getInstance()->getResource('Bwork_Config_Confighandler');
+        $moduleManager = Bwork_Core_Registry::getInstance()->getResource('Bwork_Module_Manager');
 
-		$modules = $moduleManager->getModules();
+        $modules = $moduleManager->getModules();
 
-		if(count($modules) < 1
-			|| count($url) < 1) {
-			return false;
-		}
+        if(count($modules) < 1
+            || count($url) < 1) {
+            return false;
+        }
 
-		foreach($modules as $module) {
-			if($module == $url[0]) {
-				$configModule = $config->get($module);
+        foreach($modules as $module) {
+            if($module == $url[0]) {
+                $configModule = $config->get($module);
 
-				$this->params['controller'] = isset($url[1])? $url[1] : $configModule['default_controller'];
-				$this->params['action'] = isset($url[2])? $url[2] : $configModule['default_action'];
-				$this->params['mockParams'] = array();
-				$this->params['module']  = $module;
-				return true;
-			}
-		}   	
+                $this->params['controller'] = isset($url[1])? $url[1] : $configModule['default_controller'];
+                $this->params['action'] = isset($url[2])? $url[2] : $configModule['default_action'];
+                $this->params['mockParams'] = array();
+                $this->params['module']  = $module;
+
+                return true;
+            }
+        }
     }
 
     /** 
