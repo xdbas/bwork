008a91bfa5af6c5a3feb4b1147f60d5f6914a81e
Bas van Manen <basje1@gmail.com>
2012-09-19 19:14:37 +0200

    Routing

--- libraries/bwork/router/router.php
+++ libraries/bwork/router/router.php
@@ -112,7 +112,7 @@ class Bwork_Router_Router
     /**
      * This is the main function which will handle the full routing process
      *
-     * @TODO: Route via Bwork_Router_Route instead of array(); Either cast or change completely
+     * @TODO: Route via Bwork_Router_Route instead of array()
      * @access public
      * @return void
      */
