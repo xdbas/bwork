02cbffea289b8a45649180b24c02367e8da7b447
Bas van Manen <basje1@gmail.com>
2013-01-27 20:52:53 +0100

    Implemented new config syntax

--- app/modules/admin/config/config.php
+++ app/modules/admin/config/config.php
@@ -1,10 +1,14 @@
 <?php
 
-$config['admin']['default_controller'] = 'test';
-$config['admin']['default_action']     = 'index';
+return array(
+    'admin' => array(
+        'default_controller' => 'test',
+        'default_action'     => 'index',
 
-$config['admin']['controller_path'] = 'controllers'.DIRECTORY_SEPARATOR;
-$config['admin']['model_path']      = 'models'.DIRECTORY_SEPARATOR;
-$config['admin']['vo_path']         = $config['admin']['model_path'].'vo'.DIRECTORY_SEPARATOR;
-$config['admin']['scripts_path']    = 'view'.DIRECTORY_SEPARATOR.'scripts'.DIRECTORY_SEPARATOR;
-$config['admin']['layouts_path']    = 'view'.DIRECTORY_SEPARATOR.'layouts'.DIRECTORY_SEPARATOR;
+        'controller_path' => 'controllers'.DIRECTORY_SEPARATOR,
+        'model_path'      => 'models'.DIRECTORY_SEPARATOR,
+        'vo_path'         => 'models'.DIRECTORY_SEPARATOR.'vo'.DIRECTORY_SEPARATOR,
+        'scripts_path'    => 'view'.DIRECTORY_SEPARATOR.'scripts'.DIRECTORY_SEPARATOR,
+        'layouts_path'    => 'view'.DIRECTORY_SEPARATOR.'layouts'.DIRECTORY_SEPARATOR,
+    ),
+);
