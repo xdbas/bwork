858853b1f770b3a1f643609f28b3dd7fcd7b2869
Bas van Manen <bas@developerbas.nl>
2014-04-08 12:19:42 +0200

    new autoload structure

--- libraries/bwork/loader/applicationautoloader.php
+++ libraries/bwork/loader/applicationautoloader.php
@@ -11,6 +11,7 @@
 
 require_once 'bwork/loader/autoloader.php';
 require_once 'bwork/loader/exception.php';
+
 /**
  * ApplicationAutoloader
  *
@@ -38,26 +39,27 @@ class Bwork_Loader_ApplicationAutoloader implements Bwork_Loader_Autoloader
     {
         $className = strtolower($className);
 
-        if(substr($className, -5) == 'model') {
-            $fileName  = substr($className, 0, strpos($className, 'model')) . '.php';
+        $autoload = array('model', 'vo', 'controller');
 
-            self::load($fileName, 'model');
-        }
-        elseif(substr($className, -2) == 'vo') {
-            $fileName  = substr($className, 0, strpos($className, 'vo')) . '.php';
+        if (count($autoload) > 0) {
+            foreach ($autoload as $type) {
+                if (substr($className, -strlen($type)) == $type) {
+                    $fileName = substr($className, 0, strpos($className, $type)) . '.php';
 
-            self::load($fileName, 'vo');
+                    return static::load($fileName, $type);
+                    break;
+                }
+            }
         }
-        else {
-            throw new Bwork_Loader_Exception(sprintf('Class %s could not be loaded by the autoloader.', $className));
-        }
+
+        throw new Bwork_Loader_Exception(sprintf('Class %s could not be loaded by the autoloader.', $className));
     }
 
     /**
-     * This method attempts to locate and load a file based on its type 
-     * and will give higher priority to modules and fallback on normal 
+     * This method attempts to locate and load a file based on its type
+     * and will give higher priority to modules and fallback on normal
      * Models or Value objects
-     * 
+     *
      * @access public
      * @static
      * @param string $filename
@@ -68,36 +70,38 @@ class Bwork_Loader_ApplicationAutoloader implements Bwork_Loader_Autoloader
     public static function load($filename, $type)
     {
         $registry = Bwork_Core_Registry::getInstance();
-        $config   = $registry->getResource('Bwork_Config_Confighandler');
-        $router   = $registry->getResource('Bwork_Router_Router');
+        $config = $registry->getResource('Bwork_Config_Confighandler');
+        $router = $registry->getResource('Bwork_Router_Router');
 
-        if(($module = $router->module) !== null) {
-            $filename     = str_replace($module.'_', '', $filename);
-            $pathToModule = $config->get('module_path').$module . DIRECTORY_SEPARATOR;
+        if (($module = $router->module) !== null) {
+            $filename = str_replace($module . '_', '', $filename);
+            $pathToModule = $config->get('module_path') . $module . DIRECTORY_SEPARATOR;
             $moduleConfig = $config->get($module);
-            $pathToFile   = $pathToModule.$moduleConfig[$type . '_path'];
+            $pathToFile = $pathToModule . $moduleConfig[$type . '_path'];
 
-            if(self::fileExists($pathToFile.$filename)) {
-                require_once $pathToFile.$filename;
+            if (self::fileExists($pathToFile . $filename)) {
+                require_once $pathToFile . $filename;
                 return;
             }
         }
 
         $path = $config->get($type . '_path');
-        if(self::fileExists($path.$filename)) {
-            require_once $path.$filename;
+        if (self::fileExists($path . $filename)) {
+            require_once $path . $filename;
             return;
-        }
-        else {
+        } else {
             throw new Bwork_Loader_Exception(
-                sprintf('File(%s) [%s] could not be found in any of the checked locations.', $type, $filename
-            ));
+                sprintf(
+                    'File(%s) [%s] could not be found in any of the checked locations.',
+                    $type,
+                    $filename
+                ));
         }
     }
 
     /**
      * This methods will perform checks if the given file exists and is readable
-     * 
+     *
      * @access public
      * @static
      * @param string $file
